136c5d44795d397336584948b8227c6d562918f0
JamieMichilsen020 <204927@novacollege.nl>
2026-09-02 12:24:55 +0200

    ccs link admin page

--- index.php
+++ index.php
@@ -26,13 +26,7 @@
             <a class="nav-link active" aria-current="page" href="#">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled">Disabled</a>
+            <a class="nav-link" href="#">Producten</a>
           </li>
         </ul>
       </div>
