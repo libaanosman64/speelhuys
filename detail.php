b441a637349ce7f7c5b61db28108a0f8db4f944b
libaan <libaanosman64@gmail.com>
2026-09-03 16:10:10 +0200

    Detail pagina toegevoed

---
+++ detail.php
@@ -0,0 +1,79 @@
+<?php
+
+
+
+?>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>details</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <nav class="navbar">
+            <div class="navbar">
+                <a class="navbar-brand" href="#">Speelhuys</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Producten</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="admin/index.php">Inloggen</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <div class="container" style="margin-top: 100;">
+        <div class="row">
+            <div class="col-sm">
+                <h2><b>NaamVanProduct</b><h2>
+                <img class="imageBox" src="images/sets/smartmax_safari.png"height="500" margin-left: 250; margin-right: 250;>
+                <div class="box" style="margin-top:0";>
+                    <h5>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+                        AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+                        AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+                        AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+                        AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+                    <h5>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="container" style="margin-top:460";>
+                    <img class="imageBox" src="images/logos/smartmax.png" height="50">
+                    <h4><b>$Prijs</b><h4>
+                </div>
+                <div class="box" style="margin-top:0;">
+                        <h5>Thema:     Thema<h5>
+                        <h5>Age:     Age<h5>
+                        <h5>stukken:     Stukken<h5>
+                        <h5>Hoeveelheid:     Hoeveelheid<h5>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+            
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+</body>
+
+
+
+</html>
